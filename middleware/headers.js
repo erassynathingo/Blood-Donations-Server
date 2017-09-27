ec2970794aa8aadb77a122facbd599714ffef6c0
Erastus Nathingo <erastusnathingo@Erassys-MacBook-Pro.local>
2017-09-27 17:07:01 +0200

    PC change additions

--- middleware/headers.js
+++ middleware/headers.js
@@ -10,6 +10,8 @@
 module.exports = function (req, res, next) {
   // Cross Origin headers//
   res.header('Access-Control-Allow-Origin', req.get('origin'))
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  console.log("Origin: ", req.get('origin'));
   res.header('Content-Type', 'application/json')
   res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE,PATCH')
